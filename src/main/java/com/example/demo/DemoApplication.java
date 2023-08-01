9450a891f3fac191969420e2f6a53372e825d476
Yasmine Bendjeddou <Onepiece55@outlook.fr>
2023-08-01 23:30:32 +0200

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import java.sql.*;
 import java.util.*;
 import java.util.logging.Logger;
 
 
-@SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 /*	public static void main(String[] args) {
