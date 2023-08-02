7fe041137fd9ab7fb2d2189e4a594f63eca35af0
Yasmine Bendjeddou <82144056+YasmineBend@users.noreply.github.com>
2023-08-02 15:42:40 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,43 +5,35 @@ import java.sql.*;
 import java.util.*;
 import java.util.logging.Logger;
 
+import java.util.stream.Stream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
+@SpringBootApplication
+
 
 
 public class DemoApplication {
 
-/*	public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	String sayHello() {
 		return "Welcome to my web app!";
-	}*/
-	private static final Logger log;
+	}
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(TodoRepository repository) {
+		return event->repository
+				.saveAll(Stream.of("A", "B", "C").map(name->new Todo("configuration", "congratulations, you have set up correctly!", true)).toList())
+				.forEach(System.out::println);
+	}
+	interface TodoRepository extends CrudRepository <Todo, Long> {
 
-    static {
-        System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-7s] %5$s %n");
-        log =Logger.getLogger(DemoApplication.class.getName());
-    }
-
-    public static void main(String[] args) throws Exception {
-        log.info("Loading application properties");
-        Properties properties = new Properties();
-        properties.load(DemoApplication.class.getClassLoader().getResourceAsStream("application.properties"));
-
-        log.info("Connecting to the database");
-        Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
-        log.info("Database connection test: " + connection.getCatalog());
-
-        log.info("Create database schema");
-        Scanner scanner = new Scanner(DemoApplication.class.getClassLoader().getResourceAsStream("schema.sql"));
-        Statement statement = connection.createStatement();
-        while (scanner.hasNextLine()) {
-            statement.execute(scanner.nextLine());
-		
-        log.info("Closing database connection");
-        connection.close();
-        	}
 	}
 }
 	
