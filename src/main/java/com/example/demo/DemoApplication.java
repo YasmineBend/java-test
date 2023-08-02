547d42f5b1c77b4494da79978c8bcc027a0e13b1
Yasmine Bendjeddou <82144056+YasmineBend@users.noreply.github.com>
2023-08-02 15:52:04 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,12 @@
 package com.example.demo;
 
-
-import java.sql.*;
-import java.util.*;
-import java.util.logging.Logger;
-
-import java.util.stream.Stream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
-
-
+@RestController
 
 public class DemoApplication {
 
@@ -26,14 +18,6 @@ public class DemoApplication {
 	String sayHello() {
 		return "Welcome to my web app!";
 	}
-	@Bean
-	ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(TodoRepository repository) {
-		return event->repository
-				.saveAll(Stream.of("A", "B", "C").map(name->new Todo("configuration", "congratulations, you have set up correctly!", true)).toList())
-				.forEach(System.out::println);
-	}
-	interface TodoRepository extends CrudRepository <Todo, Long> {
 
-	}
 }
 	
